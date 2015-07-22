115ec94aba360c68a245b1221bd44a7330c7df1a
GK <Gikoskos@users.noreply.github.com>
2015-07-22 20:56:53 +0300

    added a couple of new functions and fixed bugs

--- chesslib.c
+++ chesslib.c
@@ -7,10 +7,9 @@ extern void clear_buffer(void)
   while ((clbuf=getchar()) != '\n');
 }
 
-void initChessboard(ch_template chb[8][8], unsigned k, char col)	//k is row, col is column
+void initChessboard(ch_template chb[][8], unsigned k, char col)	//k is row, col is column
 { 
-
-	if (k == 0 || k == 7){
+	if (k == 0 || k == 7) {
 		if (col == 'A' || col == 'H')
 			chb[k][col - 65].current = 'R';
 		else if (col == 'B' || col == 'G')
@@ -29,7 +28,7 @@ void initChessboard(ch_template chb[8][8], unsigned k, char col)	//k is row, col
 		chb[k][col - 65].square[0] = col;
 		chb[k][col - 65].square[1] = k + '1';
 		col = col + 1;
-	} else if (k == 1 || k == 6){
+	} else if (k == 1 || k == 6) {
 		if(k == 1) 
 			chb[k][col - 65].c = WHITE;
 		else 
@@ -39,7 +38,7 @@ void initChessboard(ch_template chb[8][8], unsigned k, char col)	//k is row, col
 		chb[k][col - 65].square[0] = col;
 		chb[k][col - 65].square[1] = k + '1';
 		col = col + 1;
-	} else if (k >= 2 && k <= 5){
+	} else if (k >= 2 && k <= 5) {
 		chb[k][col - 65].c = EMPTY;
 		chb[k][col - 65].current = 'e';
 		chb[k][col - 65].occ = false;
@@ -47,27 +46,25 @@ void initChessboard(ch_template chb[8][8], unsigned k, char col)	//k is row, col
 		chb[k][col - 65].square[1] = k + '1';
 		col = col + 1;
 	}
-  
-	if (col == 'I'){
+	if (col == 'I') {
 		++k;
 		col = 'A';
 	}
-
 	if (k != 8)
 		initChessboard(chb, k, col);
 }
 
-void printBoard(ch_template chb[8][8])
+void printBoard(ch_template chb[][8])
 {
 	int i, j;
 
 	printf("    a   b   c   d   e   f   g   h  \n");
-	for (i = 0; i < 8; i++){
+	for (i = 0; i < 8; i++) {
 		printf("%d | ", i + 1);
-		for (j = 0; j < 8; j++){
-			if (chb[i][j].occ == false){
+		for (j = 0; j < 8; j++) {
+			if (chb[i][j].occ == false) {
 				printf("  | ");
-			} else{
+			} else {
 				if (chb[i][j].c == BLACK)
 					printf("%s%c%s | ", KRED, chb[i][j].current, RESET);
 				else
@@ -83,91 +80,111 @@ void printBoard(ch_template chb[8][8])
 
 bool validInput(const char *input)
 {
-	if (strcmp(input, "hlp") == 0)
+	if (strcmp(input, "help") == 0) {
+		printInstructions();
 		return false;
-	
-	if (input[0] != 'R' && input[0] != 'N' && input[0] != 'B' && input[0] != 'Q' && input[0] != 'K' && input[0] != 'P'){
+	}
+	if (input[0] != 'R' && input[0] != 'N' && input[0] != 'B' && input[0] != 'Q' && input[0] != 'K' && input[0] != 'P') {
 		printf("Not a valid Chess piece entered. Please use only R for Rook\nN for Knight\nB for Bishop\nQ for Queen\nK for King and P for Pawn\n");
 		return false;
-	} else if (input[1] < 'a' || input[1] > 'h'){
+	} else if (input[1] < 'a' || input[1] > 'h') {
 		printf("Not a valid column letter entered. Please use only a b c d e f g or h\n");
 		return false;
-	} else if (input[2] < '1' || input[2] > '8'){
+	} else if (input[2] < '1' || input[2] > '8') {
 		printf("Not a valid row entered. Please use only numbers from 1 to 8.\n");
 		return false;
 	}
-	
 	return true;
 }
 
-char *findPiece(ch_template chb[8][8], const char *input, int color)
+char *findPiece(ch_template chb[][8], const char *input, int color)
 {
-	int i, j, k, l, count;
-	char *retvalue = calloc(2, 1);
+	int i, j, k, l, count, double_attack = 3;	/*double_attack: int to check for two pieces attacking the same piece*/
+	char *retvalue = calloc(3, sizeof(*retvalue));
+	char temp[2];
 	
-	for (i = 0; i < 8; i++){
-		for (j = 0; j < 8; j++){
-			if (chb[i][j].current == input[0] && chb[i][j].c == color){
-				if (retvalue != NULL){
-					retvalue[0] = chb[i][j].square[0];
-					retvalue[1] = chb[i][j].square[1];
-				}
+	if (!retvalue)
+		exit(0);
+	retvalue[2] = '\0';
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			if (chb[i][j].current == input[0] && chb[i][j].c == color) {
+				retvalue[0] = chb[i][j].square[0];
+				retvalue[1] = chb[i][j].square[1];
 				if (input[0] == 'P'){
 					l = input[2] - '1';
 					k = input[1] - 65;
-					if (color == BLACK){ 
+					if (color == BLACK) { 
 						if ((i == 6) && (l == i - 2) && (j == k) && (chb[l][k].occ == false)) 
-							return chb[i][j].square;	//check for castling
-						if ((i-1 == 0) && (chb[i-1][j].square[0] == input[1]) && (chb[i-1][j].square[1] == input[2]) && (chb[i-1][j].occ == false)){
+							return retvalue;	//check for castling
+						if ((i-1 == 0) && (chb[i-1][j].square[0] == input[1]) && (chb[i-1][j].square[1] == input[2]) && (chb[i-1][j].occ == false)) {
 							chb[i-1][j].current = 'Q';
 							chb[i-1][j].occ = true;
 							chb[i][j].current = 'e';
 							chb[i][j].occ = false;
 							free(retvalue);
 							return "qn";
-						} else if ((i-1 == 0) && (chb[i-1][j+1].square[0] == input[1] && chb[i-1][j+1].square[1] == input[2]) && (chb[i-1][j+1].occ == true) && (chb[i-1][j+1].c != color)){
+						}
+						if ((i-1 == 0) && (chb[i-1][j+1].square[0] == input[1] && chb[i-1][j+1].square[1] == input[2]) && (chb[i-1][j+1].occ == true) && (chb[i-1][j+1].c != color)) {
 							chb[i-1][j+1].current = 'Q';
 							chb[i][j].current = 'e';
 							chb[i][j].occ = false;
 							free(retvalue);
 							return "qn";
-						} else if ((i-1 == 0) && (chb[i-1][j-1].square[0] == input[1] && chb[i-1][j-1].square[1] == input[2]) && (chb[i-1][j-1].occ == true) && (chb[i-1][j-1].c != color)){
+						}
+						if ((i-1 == 0) && (chb[i-1][j-1].square[0] == input[1] && chb[i-1][j-1].square[1] == input[2]) && (chb[i-1][j-1].occ == true) && (chb[i-1][j-1].c != color)) {
 							chb[i-1][j-1].current = 'Q';
 							chb[i][j].current = 'e';
 							chb[i][j].occ = false;
 							free(retvalue);
 							return "qn";
 						}
-						if (chb[i-1][j].occ == false){
-							if (chb[i-1][j].square[0] == input[1] && chb[i-1][j].square[1] == input[2]){
+						if (chb[i-1][j].occ == false) {
+							if (chb[i-1][j].square[0] == input[1] && chb[i-1][j].square[1] == input[2]) {
 								return retvalue;
 							}
-						} else if (chb[i-1][j+1].occ == true){
-							if (chb[i-1][j+1].square[0] == input[1] && chb[i-1][j+1].square[1] == input[2]){
-								return retvalue;
+						}
+						if (chb[i-1][j+1].occ == true) {
+							if (chb[i-1][j+1].square[0] == input[1] && chb[i-1][j+1].square[1] == input[2]) {
+								if (temp[0] != retvalue[0] && temp[1] == retvalue[1]) {
+									double_attack = true;
+									goto EXIT_LOOP;
+								}
+								double_attack = false;
+								temp[0] = retvalue[0];
+								temp[1] = retvalue[1];
 							}
-						} else if (chb[i-1][j-1].occ == true){
-							if (chb[i-1][j-1].square[0] == input[1] && chb[i-1][j-1].square[1] == input[2]){
-								return retvalue;
+						} 
+						if (chb[i-1][j-1].occ == true) {
+							if (chb[i-1][j-1].square[0] == input[1] && chb[i-1][j-1].square[1] == input[2]) {
+								if (temp[0] != retvalue[0] && temp[1] == retvalue[1]) {
+									double_attack = true;
+									goto EXIT_LOOP;
+								}
+								double_attack = false;
+								temp[0] = retvalue[0];
+								temp[1] = retvalue[1];
 							}
 						}
 					} else {
 						if ((i == 1) && (l == i + 2) && (j == k) && (chb[l][k].occ == false))
-							return chb[i][j].square;	//check for castling
-						if (((i+1) == 0) && (chb[i+1][j].square[0] == input[1] && chb[i+1][j].square[1] == input[2]) && (chb[i+1][j].occ == false)){
+							return retvalue;	//check for castling
+						if ((i+1) == 0 && chb[i+1][j].square[0] == input[1] && chb[i+1][j].square[1] == input[2] && chb[i+1][j].occ == false) {
 							chb[i+1][j].current = 'Q';
 							chb[i-1][j].occ = true;
 							chb[i][j].current = 'e';
 							chb[i][j].occ = false;
 							free(retvalue);
 							return "qn";
-						} else if (((i+1) == 0) && (chb[i+1][j+1].square[0] == input[1] && chb[i+1][j+1].square[1] == input[2]) && (chb[i+1][j+1].occ == true) && (chb[i+1][j+1].c != color)){
+						}
+						if ((i+1) == 0 && chb[i+1][j+1].square[0] == input[1] && chb[i+1][j+1].square[1] == input[2] && chb[i+1][j+1].occ == true && chb[i+1][j+1].c != color) {
 							chb[i+1][j+1].current = 'Q';
 							chb[i][j].current = 'e';
 							chb[i][j].occ = false;
 							free(retvalue);
 							return "qn";
-						} else if (((i+1) == 0) && (chb[i+1][j-1].square[0] == input[1] && chb[i+1][j-1].square[1] == input[2]) && (chb[i+1][j-1].occ == true) && (chb[i-1][j-1].c != color)){
+						}
+						if ((i+1) == 0 && chb[i+1][j-1].square[0] == input[1] && chb[i+1][j-1].square[1] == input[2] && chb[i+1][j-1].occ == true && chb[i-1][j-1].c != color) {
 							chb[i+1][j-1].current = 'Q';
 							chb[i][j].current = 'e';
 							chb[i][j].occ = false;
@@ -175,16 +192,30 @@ char *findPiece(ch_template chb[8][8], const char *input, int color)
 							return "qn";
 						}
 						if (chb[i+1][j].occ == false){
-							if (chb[i+1][j].square[0] == input[1] && chb[i+1][j].square[1] == input[2]){
+							if (chb[i+1][j].square[0] == input[1] && chb[i+1][j].square[1] == input[2]) {
 								return retvalue;
 							}
-						} else if (chb[i+1][j+1].occ == true){
-							if (chb[i+1][j+1].square[0] == input[1] && chb[i+1][j+1].square[1] == input[2]){
-								return retvalue;
+						}
+						if (chb[i+1][j+1].occ == true){
+							if (chb[i+1][j+1].square[0] == input[1] && chb[i+1][j+1].square[1] == input[2]) {
+								if (temp[0] != retvalue[0] && temp[1] == retvalue[1]) {
+									double_attack = true;
+									goto EXIT_LOOP;
+								}
+								double_attack = false;
+								temp[0] = retvalue[0];
+								temp[1] = retvalue[1];
 							}
-						} else if (chb[i+1][j-1].occ == true){
-							if (chb[i+1][j-1].square[0] == input[1] && chb[i+1][j-1].square[1] == input[2]){
-								return retvalue;
+						}
+						if (chb[i+1][j-1].occ == true){
+							if (chb[i+1][j-1].square[0] == input[1] && chb[i+1][j-1].square[1] == input[2]) {
+								if (temp[0] != retvalue[0] && temp[1] == retvalue[1]) {
+									double_attack = true;
+									goto EXIT_LOOP;
+								}
+								double_attack = false;
+								temp[0] = retvalue[0];
+								temp[1] = retvalue[1];
 							}
 						}
 					}
@@ -192,7 +223,7 @@ char *findPiece(ch_template chb[8][8], const char *input, int color)
 				} else if (input[0] == 'K') {
 					for (k = i - 1; k < i + 2; k++){
 						for (l = j - 1; l < j + 2; l++){
-							if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]){
+							if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]) {
 								return retvalue;
 							}
 						}  
@@ -202,23 +233,23 @@ char *findPiece(ch_template chb[8][8], const char *input, int color)
 					int knightcol[] = {j-1,j+1,j-2,j+2,j-2,j+2,j-1,j+1};
 					l = input[2] - '1';
 					k = input[1] - 65;
-					for (count = 0; count < 8; count++){
-						if (knightrow[count] == l && knightcol[count] == k){
+					for (count = 0; count < 8; count++) {
+						if (knightrow[count] == l && knightcol[count] == k) {
 							return retvalue;
 						}
 					}
 				} else if (input[0] == 'R' || input[0] == 'Q') {
 					k = i;
-					for (l=0; l < 8; l++){
+					for (l=0; l < 8; l++) {
 						if (l == j) continue; //to skip the piece itself
-						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]){
+						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]) {
 							return retvalue;
 						}
 					}
 					l = j;
-					for (k = 0; k < 8; k++){
+					for (k = 0; k < 8; k++) {
 						if (k == i) continue;
-						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]){
+						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]) {
 							return retvalue;
 						}
 					}
@@ -226,8 +257,8 @@ char *findPiece(ch_template chb[8][8], const char *input, int color)
 				if (input[0] == 'B' || input[0] == 'Q') {	//different if, to check for queen's diagonal moves
 					k = i - 1; 
 					l = j - 1;
-					while ((k <= 7 && k >= 0) && (l >= 0 && l <= 7)){
-						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]){
+					while ((k <= 7 && k >= 0) && (l >= 0 && l <= 7)) {
+						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]) {
 							return retvalue;
 						}
 						k--;
@@ -235,8 +266,8 @@ char *findPiece(ch_template chb[8][8], const char *input, int color)
 					}
 					k = i - 1;
 					l = j + 1;
-					while ((k >= 0 && k <= 7) && (l <= 7 && l >= 0)){
-						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]){
+					while ((k >= 0 && k <= 7) && (l <= 7 && l >= 0)) {
+						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]) {
 							return retvalue;
 						}
 						k--;
@@ -244,8 +275,8 @@ char *findPiece(ch_template chb[8][8], const char *input, int color)
 					} 
 					k = i + 1;
 					l = j - 1;
-					while ((k <= 7 && k >= 0) && (l >= 0 && l <= 0)){
-						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]){
+					while ((k <= 7 && k >= 0) && (l >= 0 && l <= 0)) {
+						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]) {
 							return retvalue;
 						}
 						k++;
@@ -253,8 +284,8 @@ char *findPiece(ch_template chb[8][8], const char *input, int color)
 					}
 					k = i + 1;
 					l = j + 1;
-					while ((k <= 7 && k >= 0) && (l <= 7 && l >= 0)){
-						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]){
+					while ((k <= 7 && k >= 0) && (l <= 7 && l >= 0)) {
+						if (chb[k][l].square[0] == input[1] && chb[k][l].square[1] == input[2]) {
 							return retvalue;
 						}
 						k++;
@@ -263,6 +294,16 @@ char *findPiece(ch_template chb[8][8], const char *input, int color)
 				}
 			}
 		}
+	}
+	EXIT_LOOP:
+	if (double_attack == true) {
+		retvalue = realloc(retvalue, 5);
+		retvalue[2] = temp[0];
+		retvalue[3] = temp[1];
+		retvalue[4] = '\0';
+		return retvalue;
+	} else if(double_attack == false) {
+		return retvalue;
 	}
 	return NULL;
 }
@@ -282,12 +323,13 @@ extern void clear_screen(void)
 {
 	char buf[1024];
 	char *str;
+	
 	tgetent(buf, getenv("TERM"));
 	str = tgetstr("cl", NULL);
 	fputs(str, stdout);
 } 
 
-bool movePiece(ch_template chb[8][8], char *plInput, char piece[2], int color)
+bool movePiece(ch_template chb[][8], char *plInput, char piece[2], int color)
 {
 	int startx, starty, endx, endy; //cords for the current tile and for the tile to move the piece to
 	endx = plInput[2] - '1';
@@ -295,8 +337,8 @@ bool movePiece(ch_template chb[8][8], char *plInput, char piece[2], int color)
 	startx = piece[1] - '1';
 	starty = piece[0] - 65;
 	
-	if (piecesOverlap(chb, startx, starty, endx, endy) == false){
-		if (chb[endx][endy].c != color){
+	if (piecesOverlap(chb, startx, starty, endx, endy) == false) {
+		if (chb[endx][endy].c != color) {
 			chb[startx][starty].occ = false;
 			chb[startx][starty].c = EMPTY;
 			chb[endx][endy].occ = true;
@@ -308,7 +350,7 @@ bool movePiece(ch_template chb[8][8], char *plInput, char piece[2], int color)
 	return false;
 }
 
-bool piecesOverlap(ch_template chb[8][8], int sx, int sy, int ex, int ey)
+bool piecesOverlap(ch_template chb[][8], int sx, int sy, int ex, int ey)
 {
 	return false;
 	
@@ -326,25 +368,71 @@ void date_filename(char *buf, int ln)
 void write_to_log(int round, FILE* logf, char *plInput, char piece[2])
 {
 	static unsigned c = 1;
-	
-	if (round == WHITE){
+
+	if (round == WHITE) {
 		fprintf(logf, "Round #%d:\tWhite moves ", c);
-	} else{
+	} else {
 		fprintf(logf, "         \tBlack moves ");
 		c++;
 	}
-	if (plInput[0] == 'P'){
+	if (plInput[0] == 'P') {
 		fprintf(logf, "pawn ");
-	} else if (plInput[0] == 'R'){
+	} else if (plInput[0] == 'R') {
 		fprintf(logf, "rook ");
-	} else if (plInput[0] == 'N'){
+	} else if (plInput[0] == 'N') {
 		fprintf(logf, "knight ");
-	} else if (plInput[0] == 'B'){
+	} else if (plInput[0] == 'B') {
 		fprintf(logf, "bishop ");
-	} else if (plInput[0] == 'Q'){
+	} else if (plInput[0] == 'Q') {
 		fprintf(logf, "queen ");
-	} else if (plInput[0] == 'K'){
+	} else if (plInput[0] == 'K') {
 		fprintf(logf, "king ");
 	}
 	fprintf(logf, "from %c%c to %c%c\n", piece[0], piece[1], plInput[1], plInput[2]);
+}
+
+char *getPlayerInput(void)
+{
+	size_t len = 0, max = 1;
+	char c, *str_in = calloc(max, sizeof(*str_in)), *str_temp = str_in;
+	
+	if (!str_in)
+		return str_in;
+	while ((c = getchar()) != '\n') {
+		str_in[len++] = c;
+		if (len == max){
+			str_temp = realloc(str_in, max*sizeof(*str_in));
+			if (!str_temp){ 
+				return str_in;
+			} else{
+				str_in = str_temp;
+			}
+			max+=1;
+		}
+	}
+	if (str_in[0] == '\n') {
+		str_in[1] = '\0';
+	} else {
+		str_in[len] = '\0';
+	}
+	return str_in;
+}
+
+char *pawnConflict(const char *pawn_pos)
+{
+	static char *temp, fpawn[3];
+	
+	printf("%s\n%s", "Did you mean to move the left Pawn or the right Pawn?",
+				  "Please specify with either 'left' or 'right': ");
+	do {
+		temp = getPlayerInput();
+	} while (strcmp(temp, "left") != 0 && strcmp(temp, "right") != 0);
+	if (strcmp(temp, "right") == 0) {
+		memcpy(fpawn, pawn_pos, 2);
+	} else {
+		fpawn[0] = pawn_pos[2];
+		fpawn[1] = pawn_pos[3];
+	}
+	fpawn[2] = '\0';
+	return fpawn;
 }
