2d8c044953fb1b051b3401513ed6813d9757ef43
GK <Gikoskos@users.noreply.github.com>
2015-08-22 22:57:55 +0300

    fix typo

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
 	KingState white_king = safe, black_king = safe;
 	FILE *logfile;
 
-	initChessboard();
+	initChessboard(chess_board);
 	date_filename(fn, s_l);
 	clear_screen();
 
