e441f20a37dc387b868740af382a61a1e4e0355d
GK <Gikoskos@users.noreply.github.com>
2015-08-27 00:43:55 +0300

    initial computer.c commit

--- computer.c
+++ computer.c
@@ -1,3 +1,11 @@
+/*********************************************************************
+ *                            computer.c                             *
+ *            computer player implementation for chesslib            *
+ *                                                                   *
+ *                 (C)2015 <cyberchiller@gmail.com>                  *
+ *                                                                   *
+ *********************************************************************/
+
 #include "chesslib.h"
 
 static unsigned total_moves;
