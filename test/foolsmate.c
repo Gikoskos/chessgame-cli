4988b8c606aef6a79b911f973a16b4fdfd7e9d33
Gikoskos <cyberchiller@gmail.com>
2015-10-12 18:44:12 +0300

    initial commit

---
+++ test/foolsmate.c
@@ -0,0 +1,50 @@
+/********************************************************************
+ *                            foolsmate.c                           *
+ *                   fool's mate to test chesslib                   *
+ *                                                                  *
+ *                 (C)2015 <cyberchiller@gmail.com>                 *
+ *                                                                  *
+ ********************************************************************/
+
+#include <chesslib.h>
+#include <chlib-cli.h>
+
+
+int main(void)
+{
+	ch_template chess_board[8][8];
+	char start_move[3], end_move[3];
+	int round = WHITE, total_rounds = 1;
+	char *fools_mate[] = {"f2f3", "e7e5", "g2g4", "d8h4"};
+
+	start_move[2] = '\0';
+	end_move[2] = '\0';
+
+	initChessboard(chess_board);
+	while (1) {
+		deleteMoves();
+		getAllMoves(chess_board, round);
+		if (BlackKing == checkmate || WhiteKing == checkmate)
+			break;
+		start_move[0] = (char)toupper(fools_mate[total_rounds-1][0]);
+		start_move[1] = (char)toupper(fools_mate[total_rounds-1][1]);
+		end_move[0] = (char)toupper(fools_mate[total_rounds-1][2]);
+		end_move[1] = (char)toupper(fools_mate[total_rounds-1][3]);
+
+		if (makeMove(chess_board, start_move, end_move, round)) {
+			round = (round == BLACK)?WHITE:BLACK;
+			total_rounds++;
+		}
+	}
+	printBoard(chess_board, 'l');
+	printf("\t\t\t***WHITE MOVES***\n");
+	printWhiteMoves();
+	printf("\n\t\t\t***BLACK MOVES***\n");
+	printBlackMoves();
+	if (!black_move_count)
+		printf("White wins!\n");
+	if (!white_move_count)
+		printf("Black wins!\n");
+	return 0;
+}
+ 
