bb5dac3b247aa3e87f5e47794d3b464386d97b95
Gikoskos <cyberchiller@gmail.com>
2015-10-21 10:57:53 +0300

    updated tests to use variadic move function

--- test/rule_tests/foolsmate.c
+++ test/rule_tests/foolsmate.c
@@ -13,31 +13,16 @@
 int main(void)
 {
 	ch_template chess_board[8][8];
-	char start_move[3], end_move[3];
-	int round = WHITE, total_rounds = 1;
-	char *fools_mate[] = {"f2f3", "e7e5", "g2g4", "d8h4"};
+	int round = WHITE;
 	clock_t start, stop;
 	double total_cpu_time;
 
-	start_move[2] = '\0';
-	end_move[2] = '\0';
-
 	start = clock();
-	initChessboard(chess_board);
-	while (1) {
-		getAllMoves(chess_board, round);
-		if (BlackKing == checkmate || WhiteKing == checkmate)
-			break;
-		start_move[0] = fools_mate[total_rounds-1][0];
-		start_move[1] = fools_mate[total_rounds-1][1];
-		end_move[0] = fools_mate[total_rounds-1][2];
-		end_move[1] = fools_mate[total_rounds-1][3];
 
-		if (makeMove(chess_board, start_move, end_move, round)) {
-			round = (round == BLACK)?WHITE:BLACK;
-			total_rounds++;
-		}
-	}
+	initChessboard(chess_board);
+	playMoves(chess_board, &round, 4, "f2f3", "e7e5", "g2g4", "d8h4");
+	getAllMoves(chess_board, round);
+
 	stop = clock();
 	total_cpu_time = ((double)(stop - start))/CLOCKS_PER_SEC;
 	printf("CPU TIME ELAPSED:%lf\n", total_cpu_time);
@@ -46,6 +31,7 @@ int main(void)
 	printWhiteMoves();
 	printf("\n\t\t\t***BLACK MOVES***\n");
 	printBlackMoves();
+
 	if (!black_move_count)
 		printf("White wins!\n");
 	if (!white_move_count)
--- test/rule_tests/scholarsmate.c
+++ test/rule_tests/scholarsmate.c
@@ -13,32 +13,16 @@
 int main(void)
 {
 	ch_template chess_board[8][8];
-	char start_move[3], end_move[3];
-	int round = WHITE, total_rounds = 1;
-	char *scholars_mate[] = {"e2e4", "e7e5", "f1c4", "g8f6", "d1h5", "b8c6", "h5f7"};
+	int round = WHITE;
 	clock_t start, stop;
 	double total_cpu_time;
-	
-
-	start_move[2] = '\0';
-	end_move[2] = '\0';
 
 	start = clock();
-	initChessboard(chess_board);
-	while (1) {
-		getAllMoves(chess_board, round);
-		if (BlackKing == checkmate || WhiteKing == checkmate)
-			break;
-		start_move[0] = scholars_mate[total_rounds-1][0];
-		start_move[1] = scholars_mate[total_rounds-1][1];
-		end_move[0] = scholars_mate[total_rounds-1][2];
-		end_move[1] = scholars_mate[total_rounds-1][3];
 
-		if (makeMove(chess_board, start_move, end_move, round)) {
-			round = (round == BLACK)?WHITE:BLACK;
-			total_rounds++;
-		}
-	}
+	initChessboard(chess_board);
+	playMoves(chess_board, &round, 7, "e2e4", "e7e5", "f1c4", "g8f6", "d1h5", "b8c6", "h5f7");
+	getAllMoves(chess_board, round);
+
 	stop = clock();
 	total_cpu_time = ((double)(stop - start))/CLOCKS_PER_SEC;
 	printf("CPU TIME ELAPSED:%lf\n\n", total_cpu_time);
@@ -47,6 +31,7 @@ int main(void)
 	printWhiteMoves();
 	printf("\n\t\t\t***BLACK MOVES***\n");
 	printBlackMoves();
+
 	if (!black_move_count)
 		printf("White wins!\n");
 	if (!white_move_count)
